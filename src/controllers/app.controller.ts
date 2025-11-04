9d2054537f1c286848a95e5f4c28956816d7e7ac
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-11-04 12:35:08 +0300

    Rename version to appVersion in getInfo method for clarity

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -9,11 +9,11 @@ export class AppController {
   constructor(private readonly store: AppStore) {}
 
   @Get('info')
-  async getInfo(): Promise<{ version: string, serverStartedAt: string }> {
-    const version: string = await firstValueFrom(this.store.select$(selectAppVersion));
+  async getInfo(): Promise<{ appVersion: string, serverStartedAt: string }> {
+    const appVersion: string = await firstValueFrom(this.store.select$(selectAppVersion));
     const serverStartedAt: string = await firstValueFrom(this.store.select$(s => s.serverStartedAt));
     return {
-      version,
+      appVersion,
       serverStartedAt,
     };
   }
