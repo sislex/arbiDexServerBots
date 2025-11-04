0c5bb5759716909c52f36986d42de6b050b06e9b
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-11-04 12:47:15 +0300

    Add botsCount to getInfo method and implement selector for bot count

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { selectAppVersion } from '../store/selectors';
+import {selectAppVersion, selectBotsCount, selectServerStartedAt} from '../store/selectors';
 import { firstValueFrom } from 'rxjs';
 import { AppStore } from '../store/app.store';
 
@@ -9,12 +9,14 @@ export class AppController {
   constructor(private readonly store: AppStore) {}
 
   @Get('info')
-  async getInfo(): Promise<{ appVersion: string, serverStartedAt: string }> {
+  async getInfo(): Promise<{ appVersion: string, serverStartedAt: string, botsCount: number }> {
     const appVersion: string = await firstValueFrom(this.store.select$(selectAppVersion));
-    const serverStartedAt: string = await firstValueFrom(this.store.select$(s => s.serverStartedAt));
+    const serverStartedAt: string = await firstValueFrom(this.store.select$(selectServerStartedAt));
+    const botsCount: number = await firstValueFrom(this.store.select$(selectBotsCount));
     return {
       appVersion,
       serverStartedAt,
+      botsCount,
     };
   }
 }
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -9,3 +9,4 @@ export const selectServerStartedAt = (s: AppState) => s.serverStartedAt;
 export const selectBotsTypes = (s: AppState): IBotTypeAndDescription[] => s.botsTypesList;
 export const selectBotsRulesList = (s: AppState): IBotsRule[] => s.botsRulesList;
 export const selectBotsList = (s: AppState): IBot[] => s.botsList;
+export const selectBotsCount = (s: AppState): number => s.botsList.length;
