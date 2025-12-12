1a1e1cf213f66933682a37b2591c1c0f9b7e02c2
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-12-12 21:39:19 +0300

    Refactor getPairsToQuote functions to use a unified parameters interface and enhance fee handling

--- src/store/stabs/bot-rule-list.stabs.ts
+++ src/store/stabs/bot-rule-list.stabs.ts
@@ -19,19 +19,34 @@ import {
   YEP
 } from './tokens.stabs';
 
+export interface IPairsToQuoteParams {
+  poolsSettings: IPoolsSettings;
+  dex?: DexId;
+  version?: PoolVersion;
+  feePpmList?: number[];
+}
 
-function getPairsToQuote(poolsSettings: IPoolsSettings, dex: DexId = 'uniswap', version: PoolVersion = 'v3'): IPairToQuote[] {
+
+function getPairsToQuote(params: IPairsToQuoteParams): IPairToQuote[] {
+  const {
+    version = 'v2',
+  } = params;
   let pairList: IPairToQuote[] = [];
   if (version === 'v2') {
-    pairList = getPairsToQuoteV2(poolsSettings, dex, version);
+    pairList = getPairsToQuoteV2(params);
   } else if (version === 'v3') {
-    pairList = getPairsToQuoteV3(poolsSettings, dex, version);
+    pairList = getPairsToQuoteV3(params);
   }
 
   return pairList;
 }
 
-function getPairsToQuoteV2(poolsSettings: IPoolsSettings, dex: DexId = 'uniswap', version: PoolVersion = 'v2'): IPairToQuote[] {
+function getPairsToQuoteV2(params: IPairsToQuoteParams): IPairToQuote[] {
+  const {
+    poolsSettings,
+    dex = 'uniswap',
+    version = 'v2',
+  } = params;
   const pairList: IPairToQuote[] = [];
   for (const amount of poolsSettings.amountList) {
     pairList.push({
@@ -48,105 +63,95 @@ function getPairsToQuoteV2(poolsSettings: IPoolsSettings, dex: DexId = 'uniswap'
   return pairList;
 }
 
-function getPairsToQuoteV3(poolsSettings: IPoolsSettings, dex: DexId = 'uniswap', version: PoolVersion = 'v3'): IPairToQuote[] {
+function getPairsToQuoteV3(params: IPairsToQuoteParams): IPairToQuote[] {
+  const {
+    poolsSettings,
+    dex = 'uniswap',
+    version = 'v3',
+  } = params;
   const pairList: IPairToQuote[] = [];
   for (const amount of poolsSettings.amountList) {
-    for (const feePpm of poolsSettings.feePpmList) {
-      pairList.push({
-        dex,
-        version,
-        tokenIn: poolsSettings.tokenIn,
-        tokenOut: poolsSettings.tokenOut,
-        amountIn: amount,
-        amountOut: amount,
-        feePpm: feePpm,
-      });
+    if (params.feePpmList) {
+      for (const feePpm of params.feePpmList) {
+        pairList.push({
+          dex,
+          version,
+          tokenIn: poolsSettings.tokenIn,
+          tokenOut: poolsSettings.tokenOut,
+          amountIn: amount,
+          amountOut: amount,
+          feePpm: feePpm,
+        });
+      }
     }
   }
   return pairList;
 }
 
-// напиши интерфейс для POOLS_USDC_WETH
 export interface IPoolsSettings {
   tokenIn: ITokenInfo;
   tokenOut: ITokenInfo;
   amountList: string[];
-  feePpmList: number[];
 }
 
 const POOLS_USDC_WETH = {
   tokenIn: USDC,
   tokenOut: WETH,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [
-    100,
-    500,
-    3000,
-    10000
-  ],
 };
 
 const POOLS_USDC_WBTC = {
   tokenIn: USDC,
   tokenOut: WBTC,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [500, 3000, 10000],
 };
 
 const POOLS_USDC_ARB = {
   tokenIn: USDC,
   tokenOut: ARB,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [500, 3000],
 };
 
 const POOLS_USDC_TMX = {
   tokenIn: USDC,
   tokenOut: TMX,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_SECH = {
   tokenIn: USDC,
   tokenOut: SECH,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_DAI = {
   tokenIn: USDC,
   tokenOut: DAI,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_YEP = {
   tokenIn: USDC,
   tokenOut: YEP,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_QODA = {
   tokenIn: USDC,
   tokenOut: QODA,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_OPUL = {
   tokenIn: USDC,
   tokenOut: OPUL,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_USDC_FARE = {
   tokenIn: USDC,
   tokenOut: FARE,
   amountList: [parseUnits("100", USDC.decimals).toString()],
-  feePpmList: [],
 };
 
 
@@ -154,205 +159,165 @@ const POOLS_WETH_USDC = {
   tokenIn: WETH,
   tokenOut: USDC,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [100, 500, 3000, 10000],
 };
 
 const POOLS_WETH_USDT = {
   tokenIn: WETH,
   tokenOut: USDT,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [100, 500, 3000, 10000],
 };
 
 const POOLS_WETH_WBTC = {
   tokenIn: WETH,
   tokenOut: WBTC,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [100, 500, 3000, 10000],
 };
 
 const POOLS_WETH_ARB = {
   tokenIn: WETH,
   tokenOut: ARB,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [500, 3000, 10000],
 };
 
 const POOLS_WETH_DAI = {
   tokenIn: WETH,
   tokenOut: DAI,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    500,
-    3000,
-    10000
-  ],
 };
 
 const POOLS_WETH_GMX = {
   tokenIn: WETH,
   tokenOut: GMX,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [3000, 10000],
 };
 
 const POOLS_WETH_LINK = {
   tokenIn: WETH,
   tokenOut: LINK,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    500,
-    3000,
-  ],
 };
 
 const POOLS_WETH_WISE = {
   tokenIn: WETH,
   tokenOut: WISE,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [],
 };
 
 const POOLS_WETH_RAIN = {
   tokenIn: WETH,
   tokenOut: RAIN,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    100,
-    10000,
-  ],
 };
 
 const POOLS_WETH_USDCE = {
   tokenIn: WETH,
   tokenOut: USDCE,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    500,
-    3000,
-  ],
 };
 
 const POOLS_WETH_PENDLE = {
   tokenIn: WETH,
   tokenOut: PENDLE,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    500,
-    3000,
-    10000,
-  ],
 };
 
 const POOLS_WETH_WSTETH = {
   tokenIn: WETH,
   tokenOut: WSTETH,
   amountList: [parseUnits("0.03", WETH.decimals).toString()],
-  feePpmList: [
-    100,
-    3000,
-  ],
 };
 
 const POOLS_WETH_USDPLUS = {
   tokenIn: WETH,
   tokenOut: USDPLUS,
   amountList: [parseUnits("0.003", WETH.decimals).toString()],
-  feePpmList: [
-    500,
-  ],
 };
 
 const POOLS_WETH_MOR = {
   tokenIn: WETH,
   tokenOut: MOR,
   amountList: [parseUnits("0.003", WETH.decimals).toString()],
-  feePpmList: [
-    3000,
-    10000,
-  ],
 };
 
 const POOLS_WETH_CRYPTO = {
   tokenIn: WETH,
   tokenOut: CRYPTO,
   amountList: [parseUnits("0.003", WETH.decimals).toString()],
-  feePpmList: [
-    10000,
-  ],
 };
 
 
 
-const pairsToQuoteUsdcWeth = getPairsToQuote(POOLS_USDC_WETH);
-const pairsToQuoteUsdcWbtc = getPairsToQuote(POOLS_USDC_WBTC);
-const pairsToQuoteUsdcArb = getPairsToQuote(POOLS_USDC_ARB);
+const pairsToQuoteV2UsdcWeth = getPairsToQuote({poolsSettings: POOLS_USDC_WETH, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcTmx = getPairsToQuote({poolsSettings: POOLS_USDC_TMX, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcSech = getPairsToQuote({poolsSettings: POOLS_USDC_SECH, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcWbtc = getPairsToQuote({poolsSettings: POOLS_USDC_WBTC, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcArb = getPairsToQuote({poolsSettings: POOLS_USDC_ARB, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcDai = getPairsToQuote({poolsSettings: POOLS_USDC_DAI, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcYep = getPairsToQuote({poolsSettings: POOLS_USDC_YEP, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcQoda = getPairsToQuote({poolsSettings: POOLS_USDC_QODA, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcOpul = getPairsToQuote({poolsSettings: POOLS_USDC_OPUL, dex: 'uniswap', version: 'v2'});
+const pairsToQuoteV2UsdcFare = getPairsToQuote({poolsSettings: POOLS_USDC_FARE, dex: 'uniswap', version: 'v2'});
 
-const pairsToQuoteWethUsdc = getPairsToQuote(POOLS_WETH_USDC);
-const pairsToQuoteWethUsdt = getPairsToQuote(POOLS_WETH_USDT);
-const pairsToQuoteWethWbtc = getPairsToQuote(POOLS_WETH_WBTC);
-const pairsToQuoteWethArb = getPairsToQuote(POOLS_WETH_ARB);
-const pairsToQuoteWethDai = getPairsToQuote(POOLS_WETH_DAI);
-const pairsToQuoteWethGmx = getPairsToQuote(POOLS_WETH_GMX);
-const pairsToQuoteWethLink = getPairsToQuote(POOLS_WETH_LINK);
-const pairsToQuoteWethRain = getPairsToQuote(POOLS_WETH_RAIN);
-const pairsToQuoteWethUsdce = getPairsToQuote(POOLS_WETH_USDCE);
-const pairsToQuoteWethPendle = getPairsToQuote(POOLS_WETH_PENDLE);
-const pairsToQuoteWethWstest = getPairsToQuote(POOLS_WETH_WSTETH);
-const pairsToQuoteWethUsdplus = getPairsToQuote(POOLS_WETH_USDPLUS);
-const pairsToQuoteWethMor = getPairsToQuote(POOLS_WETH_MOR);
-const pairsToQuoteWethCrypto = getPairsToQuote(POOLS_WETH_CRYPTO);
+const pairsToQuoteV3WethUsdc = getPairsToQuote({poolsSettings: POOLS_WETH_USDC, dex: 'uniswap', version: 'v3', feePpmList: [100, 500, 3000, 10000],});
+const pairsToQuoteV3WethUsdt = getPairsToQuote({poolsSettings: POOLS_WETH_USDT, dex: 'uniswap', version: 'v3', feePpmList: [100, 500, 3000, 10000]});
+const pairsToQuoteV3WethWbtc = getPairsToQuote({poolsSettings: POOLS_WETH_WBTC, dex: 'uniswap', version: 'v3', feePpmList: [100, 500, 3000, 10000]});
+const pairsToQuoteV3WethArb = getPairsToQuote({poolsSettings: POOLS_WETH_ARB, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000, 10000]});
+const pairsToQuoteV3WethDai = getPairsToQuote({poolsSettings: POOLS_WETH_DAI, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000, 10000]});
+const pairsToQuoteV3WethGmx = getPairsToQuote({poolsSettings: POOLS_WETH_GMX, dex: 'uniswap', version: 'v3', feePpmList: [3000, 10000]});
+const pairsToQuoteV3WethLink = getPairsToQuote({poolsSettings: POOLS_WETH_LINK, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000]});
+const pairsToQuoteV3WethRain = getPairsToQuote({poolsSettings: POOLS_WETH_RAIN, dex: 'uniswap', version: 'v3', feePpmList: [100, 10000]});
+const pairsToQuoteV3WethUsdce = getPairsToQuote({poolsSettings: POOLS_WETH_USDCE, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000]});
+const pairsToQuoteV3WethPendle = getPairsToQuote({poolsSettings: POOLS_WETH_PENDLE, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000, 10000]});
+const pairsToQuoteV3WethWstest = getPairsToQuote({poolsSettings: POOLS_WETH_WSTETH, dex: 'uniswap', version: 'v3', feePpmList: [100, 3000]});
+const pairsToQuoteV3WethUsdplus = getPairsToQuote({poolsSettings: POOLS_WETH_USDPLUS, dex: 'uniswap', version: 'v3', feePpmList: [500]});
+const pairsToQuoteV3WethMor = getPairsToQuote({poolsSettings: POOLS_WETH_MOR, dex: 'uniswap', version: 'v3', feePpmList: [3000, 10000]});
+const pairsToQuoteV3WethCrypto = getPairsToQuote({poolsSettings: POOLS_WETH_CRYPTO, dex: 'uniswap', version: 'v3', feePpmList: [10000]});
+
+const pairsToQuoteV3UsdcWeth = getPairsToQuote({poolsSettings: POOLS_USDC_WETH, dex: 'uniswap', version: 'v3',   feePpmList: [100, 500, 3000, 10000]});
+const pairsToQuoteV3UsdcWbtc = getPairsToQuote({poolsSettings: POOLS_USDC_WBTC, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000, 10000]});
+const pairsToQuoteV3UsdcArb = getPairsToQuote({poolsSettings: POOLS_USDC_ARB, dex: 'uniswap', version: 'v3', feePpmList: [500, 3000]});
 
 
-
-const pairsToQuoteUsdcWethV2 = getPairsToQuote(POOLS_USDC_WETH, 'uniswap', 'v2');
-const pairsToQuoteUsdcTmxV2 = getPairsToQuote(POOLS_USDC_TMX, 'uniswap', 'v2');
-const pairsToQuoteUsdcSechV2 = getPairsToQuote(POOLS_USDC_SECH, 'uniswap', 'v2');
-const pairsToQuoteUsdcWbtcV2 = getPairsToQuote(POOLS_USDC_WBTC, 'uniswap', 'v2');
-const pairsToQuoteUsdcArbV2 = getPairsToQuote(POOLS_USDC_ARB, 'uniswap', 'v2');
-const pairsToQuoteUsdcDaiV2 = getPairsToQuote(POOLS_USDC_DAI, 'uniswap', 'v2');
-const pairsToQuoteUsdcYepV2 = getPairsToQuote(POOLS_USDC_YEP, 'uniswap', 'v2');
-const pairsToQuoteUsdcQodaV2 = getPairsToQuote(POOLS_USDC_QODA, 'uniswap', 'v2');
-const pairsToQuoteUsdcOpulV2 = getPairsToQuote(POOLS_USDC_OPUL, 'uniswap', 'v2');
-const pairsToQuoteUsdcFareV2 = getPairsToQuote(POOLS_USDC_FARE, 'uniswap', 'v2');
-
-// const pairsToQuoteWethWiseV2 = getPairsToQuote(POOLS_WETH_WISE, 'uniswap', 'v2');
 
 
 const pairsToQuoteBot1: IPairToQuote[] = [
-  ...pairsToQuoteUsdcWethV2,
-  ...pairsToQuoteUsdcTmxV2,
-  ...pairsToQuoteUsdcSechV2,
-  ...pairsToQuoteUsdcWbtcV2,
-  ...pairsToQuoteUsdcArbV2,
-  ...pairsToQuoteUsdcDaiV2,
-  ...pairsToQuoteUsdcYepV2,
-  ...pairsToQuoteUsdcQodaV2,
-  ...pairsToQuoteUsdcOpulV2,
-  ...pairsToQuoteUsdcFareV2,
+  ...pairsToQuoteV3WethUsdc,
+  ...pairsToQuoteV3WethUsdt,
+  ...pairsToQuoteV3WethWbtc,
+  ...pairsToQuoteV3WethArb,
+  ...pairsToQuoteV3WethDai,
+  ...pairsToQuoteV3WethGmx,
+  ...pairsToQuoteV3WethLink,
+  ...pairsToQuoteV3WethRain,
+  ...pairsToQuoteV3WethUsdce,
+  ...pairsToQuoteV3WethPendle,
+  ...pairsToQuoteV3WethWstest,
+  ...pairsToQuoteV3WethUsdplus,
+  ...pairsToQuoteV3WethMor,
+  ...pairsToQuoteV3WethCrypto,
+
+
+  ...pairsToQuoteV3UsdcWeth,
+  ...pairsToQuoteV3UsdcWbtc,
+  ...pairsToQuoteV3UsdcArb,
+
+  ...pairsToQuoteV2UsdcWeth,
+  ...pairsToQuoteV2UsdcTmx,
+  ...pairsToQuoteV2UsdcSech,
+  ...pairsToQuoteV2UsdcWbtc,
+  ...pairsToQuoteV2UsdcArb,
+  ...pairsToQuoteV2UsdcDai,
+  ...pairsToQuoteV2UsdcYep,
+  ...pairsToQuoteV2UsdcQoda,
+  ...pairsToQuoteV2UsdcOpul,
+  ...pairsToQuoteV2UsdcFare,
 
 
 
-  ...pairsToQuoteUsdcWeth,
-  ...pairsToQuoteUsdcWbtc,
-  ...pairsToQuoteUsdcArb,
 
-  ...pairsToQuoteWethUsdc,
-  ...pairsToQuoteWethUsdt,
-  ...pairsToQuoteWethWbtc,
-  ...pairsToQuoteWethArb,
-  ...pairsToQuoteWethDai,
-  ...pairsToQuoteWethGmx,
-  ...pairsToQuoteWethLink,
-  ...pairsToQuoteWethRain,
-  ...pairsToQuoteWethUsdce,
-  ...pairsToQuoteWethPendle,
-  ...pairsToQuoteWethWstest,
-  ...pairsToQuoteWethUsdplus,
-  ...pairsToQuoteWethMor,
 ];
 const pairsToQuoteBot2: IPairToQuote[] = [
-  ...pairsToQuoteWethCrypto,
+  ...pairsToQuoteV3WethCrypto,
 ];
 
 
