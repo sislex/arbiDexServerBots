95b26d06a86069cccb39ccbae0b0fa585e752d04
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-12-22 15:30:40 +0300

    Refactor bot rule list: consolidate quote pairs into a single array and reduce delay between repeats

--- src/store/stabs/bot-rule-list.stabs.ts
+++ src/store/stabs/bot-rule-list.stabs.ts
@@ -7,6 +7,18 @@ import {quotesSushiUsdcOut, quotesSushiWethOut} from './sushi/v3/quotesSushiV3.s
 import {quotesSushiV2WethOut} from './sushi/v2/quotesSushiV2.stabs';
 import {quotesUsdcOut, quotesWethOut} from './uniswap/v3/quotesUniswapV3.stabs';
 
+const quotes = [
+  ...quotesWethOut,
+  ...quotesUsdcOut,
+
+  ...quotesSushiV2WethOut,
+
+  ...quotesSushiWethOut,
+  ...quotesSushiUsdcOut,
+];
+
+console.log(quotes.length);
+
 export const BotRuleListStab: IBotsRule[] = [
   {
     id: 'botRule2',
@@ -14,7 +26,7 @@ export const BotRuleListStab: IBotsRule[] = [
       botType: IBotType.TEST_BOT,
       paused: false,
       isRepeat: true,
-      delayBetweenRepeat: 100000,
+      delayBetweenRepeat: 100,
       maxJobs: 1000000,
       maxErrors: 100,
       timeoutMs: 1000,
@@ -23,15 +35,7 @@ export const BotRuleListStab: IBotsRule[] = [
       jobType: IJobType.GET_ARBITRUM_QUOTES_MULTI,
       rpcUrl: 'https://arb1.arbitrum.io/rpc',
 
-      pairsToQuote: [
-        ...quotesWethOut,
-        ...quotesUsdcOut,
-
-        ...quotesSushiV2WethOut,
-
-        ...quotesSushiWethOut,
-        ...quotesSushiUsdcOut,
-      ],
+      pairsToQuote: quotes,
 
     }
   },
