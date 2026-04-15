89c0bbe798b2d1e0a749b210c7a0c5b6cf3914ac
Nikolay <kolyatola@mail.ru>
2026-04-15 12:38:49 +0300

    log

--- src/bots/bot-runner.service.ts
+++ src/bots/bot-runner.service.ts
@@ -25,7 +25,7 @@ export class BotRunnerService {
       distinctUntilChanged(),
       withLatestFrom(this.botsList$)
     ).subscribe(([botsRulesList, botsList]) => {
-      console.log('botsRulesList.length', botsRulesList.length);
+      console.log('botsRulesList.length', botsRulesList);
       if (botsRulesList?.length) {
         const newBotsList: IBot[] = [];
         const oldBotsList: IBot[] = [];
